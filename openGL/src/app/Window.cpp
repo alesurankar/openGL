33986b853fe66cbe117b8fbd28a720de84c7ba6c
ales2026 <ale.urankar@gmail.com>
2026-04-06 15:01:23 +0200

    constructing window

--- openGL/src/app/Window.cpp
+++ openGL/src/app/Window.cpp
@@ -12,12 +12,26 @@ Window::Window(int width_in, int height_in, const char* title)
 		throw std::runtime_error("Failed to initialize GLFW");
 	}
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 	handle_ = glfwCreateWindow(width, height, title, nullptr, nullptr);
 	if (!handle_) { 
-		glfwTerminate(); throw std::runtime_error("Failed to create GLFW window"); 
+		glfwTerminate(); 
+		throw std::runtime_error("Failed to create GLFW window"); 
 	}
 
 	glfwMakeContextCurrent(handle_);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		glfwDestroyWindow(handle_);
+		glfwTerminate();
+		throw std::runtime_error("Failed to initialize GLAD");
+	}
+	glViewport(0, 0, width, height);
+	glfwSetFramebufferSizeCallback(handle_, [](GLFWwindow* win, int w, int h) {
+		glViewport(0, 0, w, h);
+		});
 }
 
 Window::~Window()
